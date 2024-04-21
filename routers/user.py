8896d9ec6063ab04268edd0822762d275c464b8e
anojaryal <anoj1810@gmail.com>
2024-04-21 23:10:36 +0545

    creating schemas for user

--- routers/user.py
+++ routers/user.py
@@ -2,4 +2,3 @@ from fastapi import APIRouter, HTTPException, Depends, status
 from sqlalchemy.orm import Session
 import database, schemas, models
 from hashing import hashing
-
---
+++ schemas.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+from typing import Optional, List
+
+class UserBase(BaseModel):
+    username: str
+    email: str
+    full_name: str
+    address: str
+    blood_group: str
+    gender: str
+    contact_no: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id:int
+
+    class Config:
+        orm_mode:True
+
+class Login(BaseModel):
+    username: str
+    password: str
