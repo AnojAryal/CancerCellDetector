0b8fdd54c795cd698efffef5933630e76afff0cd
anojaryal <anoj1810@gmail.com>
2024-04-23 14:30:14 +0545

    authentication completed

--- JWTtoken.py
+++ JWTtoken.py
@@ -2,7 +2,9 @@ from datetime import datetime, timedelta
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
-
+import database
+import models
+from sqlalchemy.orm import Session
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 # Algorithm used for JWT token encoding and decoding
@@ -22,22 +24,28 @@ def create_access_token(data: dict):
     return encoded_jwt
 
 
-def get_current_user(token: str = Depends(oauth2_scheme)):
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
         id: int = payload.get("id")
         if username is None or id is None:
-            get_user_exception()
-        return {"username": username, "id": id}
+            raise get_user_exception()
+        user = db.query(models.User).filter(models.User.id == id).first()
+        if user is None:
+            raise  get_user_exception()
+        
+        return user
     except JWTError:
         raise get_user_exception()
+
 
 
 def get_user_exception():
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not valid credentials",
+        detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
     return credentials_exception
