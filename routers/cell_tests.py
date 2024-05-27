cac73d86eaa4d538f7c28dcac380d868e5d57123
anojaryal <anoj1810@gmail.com>
2024-05-27 13:59:21 +0545

    reslult data created

--- routers/cell_tests.py
+++ routers/cell_tests.py
@@ -56,8 +56,76 @@ def create_result(result_data: schemas.ResultCreate, db: Session = Depends(get_d
 
 
 @router.get("/result/{id}")
-def get_cellTest_by_id(id: UUID, db: Session = Depends(get_db)):
+def get_testResult_by_id(id: UUID, db: Session = Depends(get_db)):
     result = db.query(models.Result).filter(models.Result.id == id).first()
     if not result:
         raise HTTPException(status_code=404, detail=f"Result with ID {id} not found")
     return result
+
+
+@router.post(
+    "/image_data/",
+    response_model=schemas.CellTestImageData,
+    status_code=status.HTTP_201_CREATED,
+)
+def test_image(
+    cellTestImage_data: schemas.CellTestImageDataCreate, db: Session = Depends(get_db)
+):
+
+    new_ImageData = models.CellTestImageData(
+        image=cellTestImage_data.image,
+        cell_test_id=cellTestImage_data.cell_test_id,
+    )
+
+    db.add(new_ImageData)
+    db.commit()
+    db.refresh(new_ImageData)
+
+    return new_ImageData
+
+
+@router.get("/image_data/{id}")
+def get_testimagedata_by_id(id: int, db: Session = Depends(get_db)):
+    test_image = (
+        db.query(models.CellTestImageData)
+        .filter(models.CellTestImageData.id == id)
+        .first()
+    )
+    if not test_image:
+        raise HTTPException(
+            status_code=404, detail=f"test_image with ID {id} not found"
+        )
+    return test_image
+
+
+@router.post(
+    "/result_data/",
+    response_model=schemas.ResultImageData,
+    status_code=status.HTTP_201_CREATED,
+)
+def test_result(
+    cellTestResult_data: schemas.ResultImageDataCreate, db: Session = Depends(get_db)
+):
+
+    new_resultData = models.ResultImageData(
+        image=cellTestResult_data.image,
+        result_id=cellTestResult_data.result_id,
+    )
+
+    db.add(new_resultData)
+    db.commit()
+    db.refresh(new_resultData)
+
+    return new_resultData
+
+
+@router.get("/result_data/{id}")
+def get_testresultdata_by_id(id: int, db: Session = Depends(get_db)):
+    test_result = (
+        db.query(models.ResultImageData).filter(models.ResultImageData.id == id).first()
+    )
+    if not test_result:
+        raise HTTPException(
+            status_code=404, detail=f"test_result with ID {id} not found"
+        )
+    return test_result
